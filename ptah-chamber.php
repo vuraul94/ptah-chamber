78fc20b3a8ea7c5dc9546e8f69796bb073a60007
Raúl Venegas <vuraul94@gmail.com>
2025-06-23 10:05:33 -0600

    add register scripts forgotten

--- ptah-chamber.php
+++ ptah-chamber.php
@@ -12,6 +12,11 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+function ptah_chamber_register_scripts(){
+    wp_register_script('swiper-script', 'https://unpkg.com/swiper/swiper-bundle.min.js', array(), '6.8.4', true);
+    wp_register_style('swiper-styles', 'https://unpkg.com/swiper/swiper-bundle.min.css', array(), '6.8.4');
+}
+
 add_action('wp_enqueue_scripts', 'ptah_chamber_register_scripts');
 add_action('elementor/editor/after_enqueue_scripts', 'ptah_chamber_register_scripts');
 
